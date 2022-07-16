56a87ba41521e39938be56e6ff2e8243dc112240
samflwr <109246278+samflwr@users.noreply.github.com>
2022-07-16 19:04:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 
+Look mah' no JS!!!!
+
+I wanted to illustrated CSS transitions with only transitions on the left and CSS animations on the right for one of my talk. The SVG was created using Adobe Illustrator Beta.
+
+If you want to see each one on their own: - the transition day/night only: https://codepen.io/stephaniewalter/pen/yLObjRW - the animation only https://codepen.io/stephaniewalter/pen/vYGmrLR
+
+*/
